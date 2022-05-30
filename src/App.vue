b62b99a14d13da730e6eb547436a03507504b708
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2022-05-30 08:20:32 +0530

    Vue HTML Binding

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>hiii {{ name }}</h1>
+  <div v-text="comp"></div>
+  <div v-html="channel"></div>
+  <div v-bind:id="testID">VBind</div>
+  <button :disabled="isDisabled">Submit</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Pradeep",
+      comp: "Delhi Chal1",
+      channel: "<b>Hello World</b>",
+      testID: "T001",
+      isDisabled: true,
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  background: rgb(207, 167, 167);
+  text-align: center;
+}
+</style>
